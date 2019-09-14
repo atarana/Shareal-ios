0a65a5d2f71941ef6ad5576063cdaa5840f97356
Kunal Sahni <sahnikunal13@yahoo.it>
2019-09-14 19:14:02 -0400

    Initial Commit

---
+++ hackmit/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  hackmit
+//
+//  Created by Kunal Sahni on 14/09/2019.
+//  Copyright © 2019 Kunal Sahni. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
